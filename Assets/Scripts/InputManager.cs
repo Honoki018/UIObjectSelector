b57bcff4e0a6119636f9539d75035fbce59a6010
Jerick Menes <jerick.menes@kooapps.com>
2017-04-21 20:03:16 +0800

    UIObjectSelector project created and finished

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InputType{
+	mouse,
+	touch
+}
+
+public class InputManager : MonoBehaviour {
+
+	public InputType inputType;
+	public GameObject selectedObject;
+
+	void Update () {
+		if (inputType == InputType.mouse) {
+			SelectObjectByMouse ();
+		} else if (inputType == InputType.touch) {
+		
+		}
+	}
+
+	void SelectObjectByMouse(){
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hitInfo;
+
+			if (Physics.Raycast (ray, out hitInfo)) {
+				/*it is adviced to create a base object for a model
+				where the actual model would be in.
+				this will provide a more managable object for you.*/
+				//this gets the gameobject where the collider is
+				GameObject hitObject = hitInfo.transform.root.gameObject;
+				SelectObject (hitObject);
+			} else {
+				ClearSelectedObject ();
+			}
+		}
+	}
+
+	void SelectObject (GameObject obj){
+		if (this.selectedObject != null) {
+			if (obj == this.selectedObject) {
+				return;
+			}
+			ClearSelectedObject ();
+		}
+
+		selectedObject = obj;
+	}
+
+	void ClearSelectedObject(){
+		if (selectedObject == null) {
+			return;
+		}
+		selectedObject = null;
+	}
+}
---
+++ Assets/Scripts/UISelectedIndicator.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISelectedIndicator : MonoBehaviour {
+
+	[Range(1.0f, 2.0f)]
+	public float padding = 1.0f;
+
+	InputManager inputManager;
+
+	void Start(){
+		inputManager = GameObject.FindObjectOfType<InputManager> ();
+		Debug.Log (inputManager.name);
+	}
+
+	void Update(){
+		if (inputManager.selectedObject == null) {
+			for (int i = 0; i < this.transform.childCount; i++) {
+				this.transform.GetChild (i).gameObject.SetActive (false);
+			}
+			return;
+		}
+		SelectObject (inputManager.selectedObject);
+	}
+
+	public void SelectObject (GameObject obj){
+
+		for (int i = 0; i < this.transform.childCount; i++) {
+			this.transform.GetChild (i).gameObject.SetActive (true);
+		}
+
+		Rect visualRect = RendererBoundsInScreenSpace (obj.GetComponent<Renderer> ());
+
+		RectTransform rectTransform = GetComponent<RectTransform> ();
+
+		rectTransform.position = new Vector2(visualRect.xMin, visualRect.yMin);
+
+		rectTransform.sizeDelta = new Vector2(visualRect.width, visualRect.height); 
+	}
+
+	static Vector3[] screenSpaceCorners;
+	static Rect RendererBoundsInScreenSpace(Renderer r) {
+		// This is the space occupied by the object's visuals
+		// in WORLD space.
+		Bounds bigBounds = r.bounds;
+
+		if(screenSpaceCorners == null)
+			screenSpaceCorners = new Vector3[8];
+
+		Camera theCamera = Camera.main;
+
+		// For each of the 8 corners of our renderer's world space bounding box,
+		// convert those corners into screen space.
+		screenSpaceCorners[0] = theCamera.WorldToScreenPoint( new Vector3( bigBounds.center.x + bigBounds.extents.x, bigBounds.center.y + bigBounds.extents.y, bigBounds.center.z + bigBounds.extents.z ) );
+		screenSpaceCorners[1] = theCamera.WorldToScreenPoint( new Vector3( bigBounds.center.x + bigBounds.extents.x, bigBounds.center.y + bigBounds.extents.y, bigBounds.center.z - bigBounds.extents.z ) );
+		screenSpaceCorners[2] = theCamera.WorldToScreenPoint( new Vector3( bigBounds.center.x + bigBounds.extents.x, bigBounds.center.y - bigBounds.extents.y, bigBounds.center.z + bigBounds.extents.z ) );
+		screenSpaceCorners[3] = theCamera.WorldToScreenPoint( new Vector3( bigBounds.center.x + bigBounds.extents.x, bigBounds.center.y - bigBounds.extents.y, bigBounds.center.z - bigBounds.extents.z ) );
+		screenSpaceCorners[4] = theCamera.WorldToScreenPoint( new Vector3( bigBounds.center.x - bigBounds.extents.x, bigBounds.center.y + bigBounds.extents.y, bigBounds.center.z + bigBounds.extents.z ) );
+		screenSpaceCorners[5] = theCamera.WorldToScreenPoint( new Vector3( bigBounds.center.x - bigBounds.extents.x, bigBounds.center.y + bigBounds.extents.y, bigBounds.center.z - bigBounds.extents.z ) );
+		screenSpaceCorners[6] = theCamera.WorldToScreenPoint( new Vector3( bigBounds.center.x - bigBounds.extents.x, bigBounds.center.y - bigBounds.extents.y, bigBounds.center.z + bigBounds.extents.z ) );
+		screenSpaceCorners[7] = theCamera.WorldToScreenPoint( new Vector3( bigBounds.center.x - bigBounds.extents.x, bigBounds.center.y - bigBounds.extents.y, bigBounds.center.z - bigBounds.extents.z ) );
+
+		// Now find the min/max X & Y of these screen space corners.
+		float min_x = screenSpaceCorners[0].x;
+		float min_y = screenSpaceCorners[0].y;
+		float max_x = screenSpaceCorners[0].x;
+		float max_y = screenSpaceCorners[0].y;
+
+		for (int i = 1; i < 8; i++) {
+			if(screenSpaceCorners[i].x < min_x) {
+				min_x = screenSpaceCorners[i].x;
+			}
+			if(screenSpaceCorners[i].y < min_y) {
+				min_y = screenSpaceCorners[i].y;
+			}
+			if(screenSpaceCorners[i].x > max_x) {
+				max_x = screenSpaceCorners[i].x;
+			}
+			if(screenSpaceCorners[i].y > max_y) {
+				max_y = screenSpaceCorners[i].y;
+			}
+		}
+
+		return Rect.MinMaxRect( min_x, min_y, max_x, max_y );
+
+	}
+}
